acb24343ab2d0eb066ceab5a0a555ee426d21fe0
Konrad Bąkowski <konrad.bakowsky@gmail.com>
2023-09-06 12:07:48 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Box from "@mui/material/Box";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Box>Hello</Box>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/index.tsx",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[hash].[ext]",
+              outputPath: "assets/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", "jsx"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      title: "Frontend",
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "/dist"),
+    compress: false,
+    host: "0.0.0.0",
+    port: 3001,
+    historyApiFallback: true,
+  },
+};
