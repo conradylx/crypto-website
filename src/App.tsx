badc37e347120d09a2948bfc7f318913bdee80e4
Konrad Bąkowski <konrad.bakowsky@gmail.com>
2023-09-07 10:54:57 +0200

    Added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import Box from "@mui/material/Box";
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Box>Hello</Box>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
